6c31a2706b8ca12e2ece266e29378f1fafafdb60
Alief Ahmad Azies <alief.ahmadazies@gmail.com>
2023-04-10 14:27:27 +0700

    feat(wishlist): add wishlist view controller

--- EcommerceApp/Module/Wishlist/WishlistViewController.swift
+++ EcommerceApp/Module/Wishlist/WishlistViewController.swift
@@ -8,23 +8,34 @@
 import UIKit
 
 class WishlistViewController: UIViewController {
-
+    
+    @IBOutlet weak var wishlistCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-//        view.backgroundColor = .systemYellow
+        
+        setupWishlistCollectionView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setupWishlistCollectionView() {
+        wishlistCollectionView.delegate = self
+        wishlistCollectionView.dataSource = self
+        wishlistCollectionView.register(UINib(nibName: "ProductCollectionCell", bundle: nil), forCellWithReuseIdentifier: ProductCollectionCell.identifier)
     }
-    */
+    
+}
 
+extension WishlistViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = wishlistCollectionView.dequeueReusableCell(withReuseIdentifier: ProductCollectionCell.identifier, for: indexPath) as? ProductCollectionCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 156, height: 260)
+    }
 }
