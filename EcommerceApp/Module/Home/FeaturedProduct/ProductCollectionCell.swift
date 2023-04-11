02bc78a6d0485cc9a8418be1c3f4ba9c1371efc4
Alief Ahmad Azies <alief.ahmadazies@gmail.com>
2023-04-11 21:36:19 +0700

    feat(featured product): add dummy data

--- EcommerceApp/Module/Home/FeaturedProduct/ProductCollectionCell.swift
+++ EcommerceApp/Module/Home/FeaturedProduct/ProductCollectionCell.swift
@@ -11,6 +11,7 @@ class ProductCollectionCell: UICollectionViewCell {
     
     static let identifier = "productCollectionCell"
 
+    @IBOutlet weak var productCollectionCellBgView: UIView!
     @IBOutlet weak var productNameLabel: UILabel!
     @IBOutlet weak var productPriceLabel: UILabel!
     @IBOutlet weak var productRatingLabel: UILabel!
@@ -19,6 +20,17 @@ class ProductCollectionCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func setupProductCollectionCellUI() {
+        productNameLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        productNameLabel.textColor = UIColor(hex: "#0C1A30FF")
+        productNameLabel.text = "TMA-2 HD Wireless"
+        productNameLabel.numberOfLines = 2
+//        productCollectionCellBgView.backgroundColor = .systemPink
+        
+        
+        
     }
 
 }
