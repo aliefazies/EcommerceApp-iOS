86dc3a00ed970e91a85c9d9880ec83cdef1deef1
Alief Ahmad Azies <alief.ahmadazies@gmail.com>
2023-05-05 14:02:53 +0700

    refactor(category): changed to 2 lines of collection view category

--- EcommerceApp/Module/Home/Category/CategoryTableCell.swift
+++ EcommerceApp/Module/Home/Category/CategoryTableCell.swift
@@ -7,6 +7,32 @@
 
 import UIKit
 
+enum ItemCategoryService: String, CaseIterable {
+    case billTopUp = "Bills & Top-Ups"
+    case travel = "Travel"
+    case electricMoney = "Electric Money"
+    case invest = "Invest"
+    case officialStore = "Official Store"
+    case payLater = "Pay Later"
+    case games = "Games"
+    case localPride = "Local Pride"
+    case freeDelivery = "Free Delivery"
+    case discount = "Discount"
+    case foods = "Food"
+    case cod = "Cash On Delivery"
+    case superMarket = "Supermarket"
+    case mall = "Mall"
+    case liveProduct = "Live Product"
+    case freeVouchers = "Free Vouchers"
+    case electornic = "Electronic"
+    case momAndKids = "Mom & Kids"
+    case sports = "Sports"
+    case phoneAndTablets = "Phones & Tablets"
+    case promoNearby = "Promo Nearby"
+    case donate = "Donate"
+    
+}
+
 class CategoryTableCell: UITableViewCell {
     
     static let identifier = "categoryTableCell"
@@ -31,8 +57,9 @@ extension CategoryTableCell: UICollectionViewDelegateFlowLayout, UICollectionVie
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = categoryCollectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionCell.identifier, for: indexPath) as? CategoryCollectionCell else { return UICollectionViewCell() }
         
-        cell.categoryImage.image = UIImage(named: dataCategoryDummy[indexPath.row].icon)
-        cell.categoryLabel.text = dataCategoryDummy[indexPath.row].name
+//        cell.categoryImage.image = UIImage(named: dataCategoryDummy[indexPath.row].icon)
+//        cell.categoryLabel.text = dataCategoryDummy[indexPath.row].name
+        cell.categoryLabel.text = ItemCategoryService.allCases[indexPath.item].rawValue
         cell.categoryLabel.font = UIFont.systemFont(ofSize: 12, weight: .light)
         cell.categoryLabel.numberOfLines = 0
         cell.categoryLabel.textAlignment = .center
@@ -40,11 +67,11 @@ extension CategoryTableCell: UICollectionViewDelegateFlowLayout, UICollectionVie
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dataCategoryDummy.count
+        return ItemCategoryService.allCases.count
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: categoryCollectionView.frame.size.width / 4, height: 60)
+        return CGSize(width: categoryCollectionView.frame.size.width / 5, height: 60)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -52,6 +79,6 @@ extension CategoryTableCell: UICollectionViewDelegateFlowLayout, UICollectionVie
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
+        return UIEdgeInsets(top: 8, left: 8, bottom: 0, right: 0)
     }
 }
